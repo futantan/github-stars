4cc6dd77e78493601bf2786a727ae4809b453172
futantan <luckytantanfu@gmail.com>
2019-04-01 21:22:37 +0800

    refactor - getStarredTotalCount to small function compose

--- src/utils/getStarredTotalCount.js
+++ src/utils/getStarredTotalCount.js
@@ -1,16 +1,24 @@
 import * as R from "ramda";
 import { parse } from "querystring";
 
-const getStarredTotalCount = R.compose(
-  parseInt,
-  R.prop("page"),
-  parse,
-  R.last,
-  R.split("?"),
+const getLastPaginationAPIUrlFromLink = R.compose(
   R.last,
   R.match(/<(.*)>/),
   R.last,
   R.split(",")
+);
+
+const getPageFromUrl = R.compose(
+  parseInt,
+  R.prop("page"),
+  parse,
+  R.last,
+  R.split("?")
+);
+
+const getStarredTotalCount = R.compose(
+  getPageFromUrl,
+  getLastPaginationAPIUrlFromLink
 );
 
 export default getStarredTotalCount;
