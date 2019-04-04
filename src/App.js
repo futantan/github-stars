e2f234e61adb626120b4f77afac77f7de941179a
Tantan Fu <luckytantanfu@gmail.com>
2019-04-04 14:26:43 +0800

    remove mock data

--- src/App.js
+++ src/App.js
@@ -1,20 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import RepoSction from './components/RepoSection.js';
 import * as R from 'ramda';
 import transformResponse from './utils/transformResponse';
-import mock from './mock.js'
+import { isLoggedIn } from './storage/storage.js';
+import { authWithGitHub } from './auth/authWithGitHub';
+import {
+  getTokenFromLocal,
+  setTokenToLocal,
+  removeTokenFromLocal
+} from './storage/storage';
+import fetchTotalCount from './request/fetchTotalCount';
+import fetchAllStarredRepos from './request/fetchAllStarredRepos';
+import { setTokenToRequestHeader } from './request/request';
 
-const App = () =>
-  R.map(
-    ([language, repos]) => (
-      <RepoSction
-        key={language}
-        language={language === 'null' ? 'Document' : language}
-        repos={repos}
-      />
-    ),
-    transformResponse(mock),
-  );
+const APP_STATUS = { Loading: 'Loading', Failed: 'Failed', Succeed: 'Succeed' };
+
+const App = () => {
+  const [response, updateResponse] = useState([]);
+  const [appStatus, updateAppStatus] = useState(APP_STATUS.Loading);
+  const markAppStatusFailed = () => updateAppStatus(APP_STATUS.Failed);
+  const markAppStatusSucceed = () => updateAppStatus(APP_STATUS.Succeed);
+  // prettier-ignore
+  const handleAppFailed = R.compose(removeTokenFromLocal, markAppStatusFailed);
+
+  useEffect(() => {
+    if (isLoggedIn()) {
+      Promise.resolve()
+
+        .then(getTokenFromLocal)
+        .then(setTokenToRequestHeader)
+        .then(fetchTotalCount)
+        .then(fetchAllStarredRepos)
+        .then(updateResponse)
+        .then(markAppStatusSucceed)
+        .catch(handleAppFailed);
+    } else {
+      authWithGitHub()
+        .then(setTokenToLocal)
+
+        .then(getTokenFromLocal)
+        .then(setTokenToRequestHeader)
+        .then(fetchTotalCount)
+        .then(fetchAllStarredRepos)
+        .then(updateResponse)
+        .then(markAppStatusSucceed);
+    }
+  }, []);
+
+  switch (appStatus) {
+    case APP_STATUS.Loading:
+      return 'loading';
+    case APP_STATUS.Failed:
+      return 'failed';
+    default:
+      return R.map(
+        ([language, repos]) => (
+          <RepoSction
+            key={language}
+            language={language === 'null' ? 'Document' : language}
+            repos={repos}
+          />
+        ),
+        transformResponse(response)
+      );
+  }
+};
 
 export default App;
--- src/storage/storage.js
+++ src/storage/storage.js
@@ -3,4 +3,5 @@ const TOKEN_KEY = 'github-token';
 export const isLoggedIn = () => !!window.localStorage.getItem(TOKEN_KEY);
 export const setTokenToLocal = ({ token }) =>
   window.localStorage.setItem(TOKEN_KEY, token);
+export const removeTokenFromLocal = () => window.localStorage.removeItem(TOKEN_KEY);
 export const getTokenFromLocal = () => window.localStorage.getItem(TOKEN_KEY);
